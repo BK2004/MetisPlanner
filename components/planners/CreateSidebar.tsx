f17fd25208509b317e79d41e5087358738ec7a87
BK2004 <thunder10.bk@gmail.com>
2023-09-15 23:23:25 -0500

    Attempt to solve bug by giving default selectedDate value of undefined and computing value inside of componenets if selectedDate is still default value

--- components/planners/CreateSidebar.tsx
+++ components/planners/CreateSidebar.tsx
@@ -12,7 +12,7 @@ const useFocus = () => {
 }
 
 export function CreateSidebar({ onSubmit }: { onSubmit: (data: { label: string, start: string, end: string, color: string }) => void }) {
-    const [selectedDate, setSelectedDate] = useState<Day>({date: 1, weekday: new Date().getDay(), month: new Date().getMonth(), year: new Date().getFullYear()});
+    const [selectedDate, setSelectedDate] = useState<Day|undefined>(undefined);
     const [fields, setFields] = useState<{ label: string, start: string, end: string }>({ label: "", start: "", end: "" });
     const [selectedColor, setSelectedColor] = useState("blue");
     const [labelRef, setLabelFocus] = useFocus();
@@ -46,8 +46,8 @@ export function CreateSidebar({ onSubmit }: { onSubmit: (data: { label: string, 
     }
 
     const selectDate = (val: Day) => {
-        const newStart = fields.start === "" ? "" : convertToEpochSeconds(val.date, val.month, val.year, Math.floor(Number(fields.start)/1000) - convertToEpochSeconds(selectedDate.date, selectedDate.month, selectedDate.year)) * 1000;
-        const newEnd = fields.end === "" ? "" : convertToEpochSeconds(val.date, val.month, val.year, Math.floor(Number(fields.end)/1000) - convertToEpochSeconds(selectedDate.date, selectedDate.month, selectedDate.year)) * 1000;
+        const newStart = fields.start === "" ? "" : convertToEpochSeconds(val.date, val.month, val.year, Math.floor(Number(fields.start)/1000) - convertToEpochSeconds(selectedDate?.date || new Date().getDate(), selectedDate?.month || new Date().getMonth(), selectedDate?.year || new Date().getFullYear())) * 1000;
+        const newEnd = fields.end === "" ? "" : convertToEpochSeconds(val.date, val.month, val.year, Math.floor(Number(fields.end)/1000) - convertToEpochSeconds(selectedDate?.date || new Date().getDate(), selectedDate?.month || new Date().getMonth(), selectedDate?.year || new Date().getFullYear())) * 1000;
 
         setSelectedDate(val);
         setFields({ label: fields.label, start: String(newStart), end: String(newEnd) });
@@ -71,7 +71,7 @@ export function CreateSidebar({ onSubmit }: { onSubmit: (data: { label: string, 
                         setFields({label: fields.label, start: e.target.value, end: fields.end });
                     }} value={fields.start} name="start" id="start" className="scroll-none appearance-none scroll-p-0 scroll-m-0 outline-0 dark:bg-neutral-750 bg-gray-100 ring-gray-400 dark:ring-neutral-900 ring-1 focus:ring-2 focus:ring-blue-600 dark:focus:ring-blue-500 ring-inset border-0 rounded-md px-3 py-2 w-full transition-all duration-100 ease-linear">
                         <option value=""></option>
-                        {getTimesForDay(selectedDate.date, selectedDate.month, selectedDate.year).map(({ value, label }) => {
+                        {getTimesForDay(selectedDate?.date || new Date().getDate(), selectedDate?.month || new Date().getMonth(), selectedDate?.year || new Date().getFullYear()).map(({ value, label }) => {
                             return (<option key={`start-${value}`} value={value}>{label}</option>);
                         })}
                     </select>
@@ -82,7 +82,7 @@ export function CreateSidebar({ onSubmit }: { onSubmit: (data: { label: string, 
                         setFields({label: fields.label, end: e.target.value, start: fields.start });
                     }} value={fields.end} name="end" id="end" className="scroll-none appearance-none scroll-p-0 scroll-m-0 outline-0 dark:bg-neutral-750 bg-gray-100 ring-gray-400 dark:ring-neutral-900 ring-1 focus:ring-2 focus:ring-blue-600 dark:focus:ring-blue-500 ring-inset border-0 rounded-md px-3 py-2 w-full transition-all duration-100 ease-linear">
                         <option value=""></option>
-                        {getTimesForDay(selectedDate.date, selectedDate.month, selectedDate.year).map(({ value, label }) => {
+                        {getTimesForDay(selectedDate?.date || new Date().getDate(), selectedDate?.month || new Date().getMonth(), selectedDate?.year || new Date().getFullYear()).map(({ value, label }) => {
                             return (<option key={`end-${value}`} value={value}>{label}</option>);
                         })}
                     </select>
